05118d6cd036810c17c02a8413af0923b7485f53
John Clark <johngclark36@gmail.com>
2017-04-03 01:33:44 -0400

    Create link group, node group, and use d3 force to create force simulation passing in the link data and node data

--- graph.js
+++ graph.js
@@ -4,28 +4,55 @@ $("document").ready(function () {
     dataType: "json"
   }).done(function (data) {
       var links = data.links
-      var countries = data.nodes
+      var nodes = data.nodes
 
-      var hash = {};
-      links.forEach((link)=> {
-        var sourceCountry = countries[link.source].country
-        var borderingCountry = countries[link.target].country
-        if (hash[sourceCountry]) {
-          hash[sourceCountry].push(borderingCountry)
-        } else {
-          hash[sourceCountry] = [borderingCountry]
-        }
+      var width = 900
+      var height = 700
+      const forceCharge = -100;
+      const linkDistance = 50;
 
-        if (hash[borderingCountry]) {
-          if (hash[borderingCountry].indexOf(sourceCountry) === -1) {
-            hash[borderingCountry].push(sourceCountry)
-          }
-        } else {
-            hash[borderingCountry] = [sourceCountry]
-          }
+      var force = d3.layout.force()
+         .size([width, height])
+         .nodes(nodes) //add nodes
+         .links(links) //add links
+         .on("tick", tick)
+         .linkDistance(linkDistance)
+         .charge(forceCharge)
+         .start();
+
+      var svg = d3.select("body").append("svg")
+        .attr("width", width)
+        .attr("height", height)
+
+        var link = svg.append("g")
+         .attr("class", "links")
+         .selectAll("line")
+         .data(links)
+         .enter().append("line")
+         .attr("stroke-width", "2px")
 
 
-      })
-      console.log(hash)
-  })
-});
+
+         var node = svg.selectAll('.node')
+         .data(force.nodes())
+         .enter().append('circle')
+         .attr('class', 'node')
+         .attr('r', 5);
+
+       node.append("title")
+           .text(function(d) { return d.country; });
+
+       function tick(e) {
+
+         node.attr('cx', function(d) { return d.x; })
+             .attr('cy', function(d) { return d.y; })
+             .call(force.drag);
+
+         link.attr('x1', function(d) { return d.source.x; })
+             .attr('y1', function(d) { return d.source.y; })
+             .attr('x2', function(d) { return d.target.x; })
+             .attr('y2', function(d) { return d.target.y; });
+
+     }
+   });
+ });
