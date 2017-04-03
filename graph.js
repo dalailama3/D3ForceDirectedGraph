f4535e145cf5592c0db1265a1be7e18b796fc73d
John Clark <johngclark36@gmail.com>
2017-04-03 17:39:10 -0400

    Change circle elements to image elements, import flag sprites to project, include flag sprites css stylesheet in index.html, use position absolute to correctly position image elements

--- graph.js
+++ graph.js
@@ -6,11 +6,11 @@ $("document").ready(function () {
       var links = data.links
       var nodes = data.nodes
 
-      const width = 800
-      const height = 700
+      const width = 900
+      const height = 800
       const radius = 6
       const forceCharge = -100;
-      const linkDistance = 50;
+      const linkDistance = 60;
 
       var force = d3.layout.force()
          .size([width, height])
@@ -21,29 +21,47 @@ $("document").ready(function () {
          .charge(forceCharge)
          .start();
 
-      var svg = d3.select("body").append("svg")
-        .attr("width", width)
-        .attr("height", height)
+       var graph = d3.select('.graph')
 
-        var link = svg.append("g")
-         .attr("class", "links")
-         .selectAll("line")
-         .data(links)
-         .enter().append("line")
-         .attr("stroke-width", "2px")
+        var svg = graph.append("svg")
+          .attr("width", width)
+          .attr("height", height)
 
 
+        var link = svg.selectAll('.link')
+          .data(data.links)
+          .enter()
+          .append('line')
+          .attr('class', 'link');
 
-         var node = svg.selectAll('.node')
-         .data(force.nodes())
-         .enter().append('circle')
-         .attr('class', 'node')
-         .attr('r', function (d) {
-           return d.weight * 1.3;
-         })
+
+
+         var node = graph.select('.flagbox').selectAll("img")
+           .data(force.nodes())
+           .enter().append("img")
+          //  .attr("class", "node")
+           .attr("class", function (d) {
+             return "flag" + " flag-" + d.code;
+           })
+
+        //  .attr('r', function (d) {
+        //    return d.weight/5 * 6;
+        //  })
 
        node.append("title")
            .text(function(d) { return d.country; });
+
+      //  var images = node.append("svg:image")
+       //
+      //  .attr("xlink:href",  "./flags.png")
+      //   .attr("class", function (d) {
+      //     return "node flag" + " flag-" + d.code;
+      //   })
+      //  .attr("x", function(d) { return -25;})
+      //  .attr("y", function(d) { return -25;})
+      //  .attr("height", 11)
+      //  .attr("width", 16);
+
 
        function tick(e) {
 
@@ -51,11 +69,16 @@ $("document").ready(function () {
              .attr('cy', function(d) { return d.y = Math.max(radius, Math.min(height - radius, d.y)); })
              .call(force.drag);
 
+         node.style('left', d => (d.x - 8) + "px")
+         .style('top', d => (d.y - 5) + "px");
+
          link.attr('x1', function(d) { return d.source.x; })
              .attr('y1', function(d) { return d.source.y; })
              .attr('x2', function(d) { return d.target.x; })
              .attr('y2', function(d) { return d.target.y; });
 
      }
+
    });
+
  });
